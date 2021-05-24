4ec3e3a115d742eb576b60de0ef711f9d2689c21
LaupWing <loc_strict@hotmail.com>
2021-05-24 23:15:11 +0200

    update

--- ReactMeals/src/components/Layout/Header/Header.js
+++ ReactMeals/src/components/Layout/Header/Header.js
@@ -1,4 +1,4 @@
-import image from '../../assets/meals.jpg'
+import image from '../../../assets/meals.jpg'
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton/HeaderCartButton'
 
