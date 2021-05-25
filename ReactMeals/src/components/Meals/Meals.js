2a1b483cde6abd7d328adcd0fd6a4838d8969a0c
LaupWing <loc_strict@hotmail.com>
2021-05-25 15:13:13 +0200

    134

---
+++ ReactMeals/src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from './MealsSummary/MealsSummary'
+import AvailableMeals from './AvailableMeals/AvailableMeals'
+
+const Meals = () =>{
+   return <>
+      <MealsSummary/>
+      <AvailableMeals/>
+   </>
+}
+
+export default Meals
