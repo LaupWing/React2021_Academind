f7d39baaf8c76939a645ffbaa335580926913248
LaupWing <loc_strict@hotmail.com>
2021-05-26 18:47:21 +0200

    141

--- ReactMeals/src/App.js
+++ ReactMeals/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/UI/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
    const [cartIsShown, setCartIsShown] = useState(false)
@@ -13,7 +14,7 @@ function App() {
       setCartIsShown(false)
    }
    return (
-      <>
+      <CartProvider>
          {cartIsShown && <Cart onClose={hideCartHandler}/>}
          <Header 
             onShowCart={showCartHandler}
@@ -21,7 +22,7 @@ function App() {
          <main>
             <Meals/>
          </main>
-      </>
+      </CartProvider>
    );
 }
 
