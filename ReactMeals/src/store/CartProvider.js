24b03bff8043e674843f00b35b2087bd72ae6190
LaupWing <loc_strict@hotmail.com>
2021-06-03 19:34:43 +0200

    146

--- ReactMeals/src/store/CartProvider.js
+++ ReactMeals/src/store/CartProvider.js
@@ -11,6 +11,24 @@ const cartReducer = (state, action)=>{
       const updatedItems = [...state.items, action.value]
       const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount)
 
+      const existingCartIndex = state.items.findIndex(item => item.id === action.item.id)
+
+      const existingCartItem = state.items[existingCartIndex]
+      let updateItem
+      let updatedItems
+
+      if(existingCartItem){
+         updatedItem = {
+            ...existingCartItem,
+            amount: existingCartItem + 1
+         }
+         updatedItems = [...state.items]
+         updatedItems[existingCartItem] = updateItem
+      }else{
+         updatedItem = {...action.item}
+         updatedItems = state.items.concat(action.item)
+      }
+
       return {
          items: updatedItems,
          totalAmount: updatedTotalAmount
@@ -39,7 +57,7 @@ const CartProvider = props => {
    }
 
    return <CartContext.Provider value={cartContext}>
-   {props.children}
+      {props.children}
    </CartContext.Provider>
 }
 
