71b3546e9c1f05ff6d67d713a868c6f76e6b6dbe
LaupWing <loc_strict@hotmail.com>
2021-06-03 18:33:49 +0200

    143

--- ReactMeals/src/store/CartProvider.js
+++ ReactMeals/src/store/CartProvider.js
@@ -1,13 +1,39 @@
+import { useReducer } from 'react'
 import CartContext from './cart-context'
 
-const CartProvider = props => {
+const defaultCartState = {
+   items: [],
+   totalAmount: 0
+}
 
-   const addItemToCartHandler = item =>{}
+const cartReducer = (state, action)=>{
+   if(action.type === 'ADD'){
+      const updatedItems = [...state.items, action.value]
+      const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount)
+
+      return {
+         items: updatedItems,
+         totalAmount: updatedTotalAmount
+      }
+   }
+
+   return defaultCartState
+}
+
+const CartProvider = props => {
+   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+   const addItemToCartHandler = item =>{
+      dispatchCartAction({
+         type: 'ADD',
+         value: item
+      })
+   }
    const removeItemFromCartHandler = id =>{}
 
    const cartContext = {
-      items:[],
-      totalAmount: 0,
+      items: cartState.items,
+      totalAmount: cartState.totalAmount,
       addItem: addItemToCartHandler,
       removeItem: removeItemFromCartHandler
    }
